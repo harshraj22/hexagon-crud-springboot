67912debac87007feccba200ab422cddde579123
harshraj22 <harshraj22aug@gmail.com>
2023-10-15 18:41:25 +0530

    Added Liquibase
    Trying to add docker-compose for managing the db

---
+++ src/main/resources/db/changeset/crateAuthCreds.sql
@@ -0,0 +1,5 @@
+CREATE TABLE auth_creds (
+    username VARCHAR(40) NOT NULL,
+    password VARCHAR(40) NOT NULL,
+    PRIMARY KEY (username)
+);
---
+++ src/main/resources/db/changeset/createUserProfile.sql
@@ -0,0 +1,8 @@
+CREATE TABLE user_profile (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(40) NOT NULL,
+    name VARCHAR(40) NOT NULL,
+    email VARCHAR(40) NOT NULL,
+    age INTEGER NOT NULL,
+    FOREIGN KEY (username) REFERENCES auth_creds(username)
+);
